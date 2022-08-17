067919502671eb242fd8f3d968006010d4a2d91b
Elteyab Hassan <Mr.elteb@gmail.com>
2022-08-17 15:04:46 +0200

    Fix domain

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(255);
+        Schema::defaultStringLength(125);
     }
 }
